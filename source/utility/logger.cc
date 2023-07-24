cd93c68ee50301ec59375ed4efa6699a76c580d6
张思源 <974483053@qq.com>
2023-07-24 17:31:25 +0800

    feat:优化日志逻辑并新增格式化输出功能

--- source/utility/logger.cc
+++ source/utility/logger.cc
@@ -1,39 +1,31 @@
 #include "logger.h"
+
+#include <iostream>
+#include <string>
+
 #include "lib/magic_enum/magic_enum.hpp"
 #include "lib/opentime/opentime.h"
-#include <string>
-#include <iostream>
-#include <stdio.h>
-
-Logger::Logger() {
-  cout << greeting << endl;
-};
-
-Logger& Logger::SetFunctionCallInfo(string call_file, int call_line, string call_function) {
-  call_file_ = call_file;
-  call_line_ = to_string(call_line);
-  call_function_ = call_function;
-  return *this;
-}
 
 void Logger::Init(LEVEL level) {
   level_ = level;
-  output("\n=============== Start logging ===============", LEVEL::WELCOME);
+  cout << greeting_ << endl;
 }
 
 void Logger::Init(LEVEL level, string log_file_path) {
   is_write_to_log_file_ = true;
-  output_file_stream_.open(log_file_path, ios::out | ios::app); // 打开输出文件
+  output_file_stream_.open(log_file_path, ios::out | ios::app);  // 打开输出文件
   Init(level);
 }
 
 Logger::~Logger() { output_file_stream_.close(); }
 
-void Logger::output(string text, LEVEL act_level) {
-  string output_content = GetCurrentTime() + " " + call_file_ + ":" +
-                          call_line_ + " " + call_function_ + " [" +
+void Logger::Output(string call_file, int call_line, string call_function,
+                    LEVEL act_level, string message) {
+  OpenTime open_time;
+  string output_content = open_time.toString() + " " + call_file + ":" +
+                          to_string(call_line) + " " + call_function + " [" +
                           string(magic_enum::enum_name(act_level)) + "] - " +
-                          text + "\n";
+                          message + "\n";
   if (this->level_ <= act_level) {
     // 当前等级设定的等级才会显示在终端，且不能是只文件模式
     cout << output_content;
@@ -43,17 +35,3 @@ void Logger::output(string text, LEVEL act_level) {
     output_file_stream_.flush();
   }
 }
-
-string Logger::GetCurrentTime() {
-  OpenTime open_time;
-  return open_time.toString();
-}
-
-void Logger::DEBUG(string text) { this->output(text, LEVEL::DEBUG); }
-
-void Logger::INFO(string text) { this->output(text, LEVEL::INFO); }
-
-void Logger::WARNING(string text) { this->output(text, LEVEL::WARNING); }
-
-void Logger::ERROR(string text) { this->output(text, LEVEL::ERROR); }
-
--- source/utility/logger.h
+++ source/utility/logger.h
@@ -1,63 +1,71 @@
 #pragma once
 
+#include <format>
 #include <fstream>
 using namespace std;
 
 class Logger {
-public:
+ public:
   // 日志等级
-  enum class LEVEL { DEBUG, INFO, WARNING, ERROR, WELCOME };
-private:
-  ofstream output_file_stream_; // 将日志输出到文件的流对象
-  LEVEL level_;                 // 日志等级
-  string call_file_;
-  string call_line_;
-  string call_function_;
+  enum class LEVEL { DEBUG, INFO, WARNING, ERROR };
+
+ private:
+  ofstream output_file_stream_;  // 将日志输出到文件的流对象
+  LEVEL level_;                  // 日志等级
   bool is_write_to_log_file_ = false;
 
  private:
-  Logger();
-  void output(string text, LEVEL act_level); // 输出行为
-  string GetCurrentTime();
   ~Logger();
 
-public:
-  static Logger &GetInstance() {
+ public:
+  static Logger& GetInstance() {
     static Logger instance;
     return instance;
   }
-  Logger& SetFunctionCallInfo(string call_file, int call_line, string call_function);
   void Init(LEVEL level);
   void Init(LEVEL level, string log_file_path);
-  void DEBUG(string text);
-  void INFO(string text);
-  void WARNING(string text);
-  void ERROR(string text);
 
-private:
-  string greeting =
-    "///////////////////////////////////////////////////////////////\n"
-    "//                          _ooOoo_                          //\n"
-    "//                         o8888888o                         //\n"
-    "//                         88\" . \"88                         //\n"
-    "//                         (| ^_^ |)                         //\n"
-    "//                         O\\  =  /O                         //\n"
-    "//                      ____/`---'\\____                      //\n"
-    "//                    .'  \\\\|     |//  `.                    //\n"
-    "//                   /  \\\\|||  :  |||//  \\                   //\n"
-    "//                  /  _||||| -:- |||||-  \\                  //\n"
-    "//                  |   | \\\\\\  -  /// |   |                  //\n"
-    "//                  | \\_|  ''\\---/''  |   |                  //\n"
-    "//                  \\  .-\\__  `-`  ___/-. /                  //\n"
-    "//                ___`. .'  /--.--\\  `. . ___                //\n"
-    "//              .\"\"'<  `.___\\_<|>_/___.'  >'\"\".              //\n"
-    "//            | | : `- \\\\`.;`\\ _ /`;.`/ - ` : | |            //\n"
-    "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /            //\n"
-    "//      =======`-.____`-.___\\_____/___.-`____.-'=======      //\n"
-    "//                          `=---='                          //\n"
-    "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^      //\n"
-    "//             佛祖保佑     永不宕机     永无BUG             //\n"
-    "///////////////////////////////////////////////////////////////\n";
+  void Output(string call_file, int call_line, string call_function,
+              LEVEL act_level, string message);  // 输出行为
+
+ private:
+  string greeting_ =
+      "///////////////////////////////////////////////////////////////\n"
+      "//                          _ooOoo_                          //\n"
+      "//                         o8888888o                         //\n"
+      "//                         88\" . \"88                         //\n"
+      "//                         (| ^_^ |)                         //\n"
+      "//                         O\\  =  /O                         //\n"
+      "//                      ____/`---'\\____                      //\n"
+      "//                    .'  \\\\|     |//  `.                    //\n"
+      "//                   /  \\\\|||  :  |||//  \\                   //\n"
+      "//                  /  _||||| -:- |||||-  \\                  //\n"
+      "//                  |   | \\\\\\  -  /// |   |                  //\n"
+      "//                  | \\_|  ''\\---/''  |   |                  //\n"
+      "//                  \\  .-\\__  `-`  ___/-. /                  //\n"
+      "//                ___`. .'  /--.--\\  `. . ___                //\n"
+      "//              .\"\"'<  `.___\\_<|>_/___.'  >'\"\".              //\n"
+      "//            | | : `- \\\\`.;`\\ _ /`;.`/ - ` : | |            //\n"
+      "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /            //\n"
+      "//      =======`-.____`-.___\\_____/___.-`____.-'=======      //\n"
+      "//                          `=---='                          //\n"
+      "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^      //\n"
+      "//             佛祖保佑     永不宕机     永无BUG             //\n"
+      "///////////////////////////////////////////////////////////////\n";
 };
+#define log Logger::GetInstance()
+#define LOGINFO(act_level, format_message, ...)       \
+  Output(__FILE__, __LINE__, __FUNCTION__, act_level, \
+         format(format_message __VA_OPT__(, ) __VA_ARGS__))
 
-#define log Logger::GetInstance().SetFunctionCallInfo(__FILE__, __LINE__, __FUNCTION__)
+#define DEBUG(format_message, ...) \
+  LOGINFO(Logger::LEVEL::DEBUG, format_message, __VA_ARGS__)
+
+#define INFO(format_message, ...) \
+  LOGINFO(Logger::LEVEL::INFO, format_message, __VA_ARGS__)
+
+#define WARNING(format_message, ...) \
+  LOGINFO(Logger::LEVEL::WARNING, format_message, __VA_ARGS__)
+
+#define ERROR(format_message, ...) \
+  LOGINFO(Logger::LEVEL::ERROR, format_message, __VA_ARGS__)
